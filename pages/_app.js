e3b9587597df1ca98aadc0db0d020b60762a3c7a
Radek Sienkiewicz <mail@velvetshark.com>
2023-03-26 03:20:24 +0200

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
